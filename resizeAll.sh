a7292c9fa58b6a941a6ef9f45d46924136f91ecb
Rémi Ratajczak <Remi.Ratajczak@gmail.com>
2018-04-25 11:42:20 +0200

    Update resizeAll.sh

--- resizeAll.sh
+++ resizeAll.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+  #!/bin/bash
 #Resize all images in a Folder
 
 #Get a file as an input - if it is not a file it will not work
 [ $# -ge 1 -a -f "$1" ] && input="$1" || input="-"
 cat $input
 
-# The exclamation point after the size force the size without respectinf aspect ratio - remove it if you want to respect aspect ration
+# The exclamation point after the size force the size without respectinf aspect ratio - remove it if you want to respect aspect ratio
 for i in input/*.JPG;
 do
-convert $i -resize 28x28! $(basename $i .JPG).JPG;
+  convert $i -resize 28x28! $(basename $i .JPG).JPG;
 done
